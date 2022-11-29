8dc47d63ff8e936d70d3a9aa69a0daf0c1b0691d
Artem Galay <iostema@mail.ru>
2022-11-29 14:10:15 +0300

    Create MainTabBarViewController.

---
+++ Netflix Clone/Controllers/DownloadlsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadlsViewController.swift
+//  Netflix Clone
+//
+//  Created by Артем Галай on 29.11.22.
+//
+
+import UIKit
+
+class DownloadlsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Артем Галай on 29.11.22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
