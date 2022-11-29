328bb9ebfb86987798a12ba82f0e82397a6ad3cd
Artem Galay <iostema@mail.ru>
2022-11-29 15:14:23 +0300

    Create HomeViewController.

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -9,10 +9,62 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private lazy var homeFeedTable: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(CollectionViewTableViewCell.self,
+                           forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupHierarchy()
+        setupLayout()
         view.backgroundColor = .systemBackground
+    }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(homeFeedTable)
+    }
+
+    private func setupLayout() {
+
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        20
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        40
     }
 }
